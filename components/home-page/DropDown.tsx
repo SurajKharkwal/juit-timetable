7d2a011299a1d967ab4cb01f97781dc6e5861efd
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-02-27 16:50:42 +0530

    Fix: eslint error

--- components/home-page/DropDown.tsx
+++ components/home-page/DropDown.tsx
@@ -13,12 +13,12 @@ const DropDown = () => {
             <DropdownMenu aria-label="Static Actions">
                 <DropdownItem key="new">
                     <a href="https://github.com/shorya-1012" className='w-full h-full'>
-                        Shorya's
+                        {"Shorya's"}
                     </a>
                 </DropdownItem>
                 <DropdownItem key="copy">
                     <a href="https://github.com/SurajKharkwal" className='w-full h-full'>
-                        Suraj's
+                        {"Suraj's"}
                     </a>
                 </DropdownItem>
                 <DropdownItem key="delete" className="text-danger" color="danger">
