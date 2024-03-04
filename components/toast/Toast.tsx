c4aa04954394e143b3a9a54f3c391663e1ad9f2e
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-03-04 21:37:12 +0530

    fix: Toast ui

--- components/toast/Toast.tsx
+++ components/toast/Toast.tsx
@@ -1,30 +1,29 @@
-"use client"
-import * as React from 'react';
-import * as Toast from '@radix-ui/react-toast';
-import './styles.css';
+"use client";
+import * as React from "react";
+import * as Toast from "@radix-ui/react-toast";
+import { IoCloseCircle } from "react-icons/io5";
+import "./styles.css";
 
 type Props = {
   open: boolean;
   setOpen: (value: boolean) => void;
-}
+};
 
-const ToastComponent = ({open, setOpen}: Props) => {
+const ToastComponent = ({ open, setOpen }: Props) => {
   return (
     <Toast.Provider swipeDirection="right">
       <Toast.Root className="ToastRoot" open={open} onOpenChange={setOpen}>
-        <div className=''>
-          <Toast.Title className="ToastTitle">
-            <h1 className='text-red-700 font-extrabold '>No Data Found</h1>
-          </Toast.Title>
-          <Toast.Description asChild>
-            <p className='text-red-500 font-semibold'>Please Check, Course Name & Batch Name again ...</p>
-          </Toast.Description>
+        <div className="">
+          <h1 className="flex items-center font-semibold text-red-500 justify-start gap-2 ">
+            <IoCloseCircle  className="text-2xl text-red-500 "/>
+            Operation Falied !
+          </h1>
+          <p>Please, Check the Bactch or Course Name</p>
         </div>
       </Toast.Root>
       <Toast.Viewport className="ToastViewport" />
     </Toast.Provider>
   );
 };
-
 
 export default ToastComponent;
