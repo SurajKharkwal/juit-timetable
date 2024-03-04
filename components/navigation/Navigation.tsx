29add5fb11cfc4455922f93d1e2c5da80e5cc168
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-03-05 04:00:57 +0530

    Fix: Nav ui error

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -17,7 +17,7 @@ const Navigation = () => {
       <div className="max-w-[100vw] md:max-w-[90vw] flex items-center justify-end pl-4 pr-4 w-full h-full ">
         <Tooltip content="Change Theme">
           <Button
-            className="text-2xl bg-transparent max-md:translate-x-[300%]"
+            className="text-2xl bg-transparent "
             isIconOnly
           >
             {isDark ? (
@@ -27,7 +27,7 @@ const Navigation = () => {
             )}
           </Button>
         </Tooltip>
-        <p className='opacity-0 sm:opacity-100 text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"'>
+        <p className='max-md:hidden text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold"'>
           Check out our
         </p>
         <DropDown />
