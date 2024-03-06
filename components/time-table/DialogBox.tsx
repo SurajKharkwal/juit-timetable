f6daaa596c890cbde42f1ccb1da1c1be7272df30
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-03-06 21:37:04 +0530

    Fix: UI bugs (Dialog box)

--- components/time-table/DialogBox.tsx
+++ components/time-table/DialogBox.tsx
@@ -1,69 +1,90 @@
-import React, { useState } from 'react'
-import AutoComplete from '../auto-complete/AutoCompelete'
-import { Input } from '@nextui-org/input'
-import { Button } from '@nextui-org/button';
+import React, { useState } from "react";
+import AutoComplete from "../auto-complete/AutoCompelete";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
 type Props = {
-    setShowDialogBox(value: boolean): void;
-    fetchDataFunction: (course: string, batch: string) => void
+  setShowDialogBox(value: boolean): void;
+  fetchDataFunction: (course: string, batch: string) => void;
 };
 
-
 const DialogBox = ({ setShowDialogBox, fetchDataFunction }: Props) => {
-    const [inputData, setInputData] = useState<{ course: string; batch: string }>({ course: "", batch: "" });
-    const [errorMessage, setErrorMessage] = useState("");
+  const [inputData, setInputData] = useState<{ course: string; batch: string }>(
+    { course: "", batch: "" }
+  );
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const handleCourseChange = (selectedCourse: string) => {
-        setInputData({ ...inputData, course: selectedCourse });
-    };
-    const handleBatchChange = (batch: string) => {
-        let formatedBatch: string = "";
-        batch.split("").forEach(element => {
-            if (element !== " " && element !== "-")
-                formatedBatch += element.toUpperCase();
-        });
-        setInputData({ ...inputData, batch: formatedBatch });
-    };
+  const handleCourseChange = (selectedCourse: string) => {
+    setInputData({ ...inputData, course: selectedCourse });
+  };
+  const handleBatchChange = (batch: string) => {
+    let formatedBatch: string = "";
+    batch.split("").forEach((element) => {
+      if (element !== " " && element !== "-")
+        formatedBatch += element.toUpperCase();
+    });
+    setInputData({ ...inputData, batch: formatedBatch });
+  };
 
-    const handleSubmit = async () => {
-        if (!inputData.course) {
-            setErrorMessage("Please select a course");
-            return;
-        }
+  const handleSubmit = async () => {
+    if (!inputData.course) {
+      setErrorMessage("Please select a course");
+      return;
+    }
 
-        if (!inputData.batch) {
-            setErrorMessage("Please enter the Batch");
-            return;
-        }
+    if (!inputData.batch) {
+      setErrorMessage("Please enter the Batch");
+      return;
+    }
 
-        fetchDataFunction(inputData.course, inputData.batch)
+    fetchDataFunction(inputData.course, inputData.batch);
 
-        setShowDialogBox(false)
-    };
+    setShowDialogBox(false);
+  };
 
-    return (
-        <div className='w-full h-full absolute backdrop-blur-lg z-10  flex items-center justify-center '>
-            <form onSubmit={handleSubmit} className=' text-xl gap-4 flex items-start border-2 border-white/30 shadow-2xl bg-white/10 h-[40vh] rounded-lg p-8 justify-center flex-col'>
-                <h1 className='text-blue-500 font-semibold'>Edit Form</h1>
-                <div>
-                    <h1>Select the Course</h1>
-                    <AutoComplete setCourse={handleCourseChange} />
-                </div>
-                <div className='w-full'>
-                    <h1>Select Batch</h1>
-                    <Input onChange={(e) => handleBatchChange(e.target.value)} />
-                </div>
-                <p className='text-red-500 w-full items-center justify-center flex font-extralight'>{errorMessage}</p>
-                <div className='w-full gap-2 flex items-center justify-center '>
-                    <Button type='submit' onClick={handleSubmit} className='bg-blue-500 text-white'>Submit</Button>
-                    <Button onClick={() => setShowDialogBox(false)} className='border-blue-500 border-2 bg-transparent text-blue-300'>Close</Button>
-                </div>
-            </form>
-            <section className='absolute bottom-2 flex items-center justify-center flex-col'>
-                <h6 className=' font-extralight'>created by</h6>
-                <p className='font-bold text-blue-500'>SURAJ & SHORYA</p>
-            </section>
+  return (
+    <div className="w-full h-full absolute backdrop-blur-lg z-10 ">
+      <style jsx global>
+        {"body { width: 100%; height: 100vh; overflow: hidden; }"}
+      </style>
+
+      <form
+        onSubmit={handleSubmit}
+        className=" sticky top-[50%] left-[50%] -translate-y-[50%]  text-xl gap-4 flex items-start border-2 border-white/30 shadow-2xl bg-white/10 h-[40vh] rounded-lg p-8 justify-center flex-col"
+      >
+        <h1 className="text-blue-500 font-semibold">Edit Form</h1>
+        <div>
+          <h1>Select the Course</h1>
+          <AutoComplete setCourse={handleCourseChange} />
         </div>
-    )
-}
-export default DialogBox
+        <div className="w-full">
+          <h1>Select Batch</h1>
+          <Input onChange={(e) => handleBatchChange(e.target.value)} />
+        </div>
+        <p className="text-red-500 w-full items-center justify-center flex font-extralight">
+          {errorMessage}
+        </p>
+        <div className="w-full gap-2 flex items-center justify-center ">
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            className="bg-blue-500 text-white"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setShowDialogBox(false)}
+            className="border-blue-500 border-2 bg-transparent text-blue-300"
+          >
+            Close
+          </Button>
+        </div>
+      </form>
+      <section className="absolute w-full top-[95dvh] flex items-center justify-center flex-col">
+        <h6 className=" font-extralight">created by</h6>
+        <p className="font-bold text-blue-500">SURAJ & SHORYA</p>
+      </section>
+    </div>
+  );
+};
+export default DialogBox;
--- components/time-table/TimeTableUI.tsx
+++ components/time-table/TimeTableUI.tsx
@@ -1,96 +1,120 @@
-"use client"
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue, Popover, } from "@nextui-org/react";
-import React, { useState } from 'react'
+"use client";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Popover,
+} from "@nextui-org/react";
+import React, { useState } from "react";
 import PopOver from "./PopOver";
 
-
 type Props = {
-    rows: {
-        key: string;
-    }[];
+  rows: {
+    key: string;
+  }[];
 };
 
-
 const TimeTableUI = ({ rows }: Props) => {
+  const [value, setValue] = useState("");
+  const columns = [
+    {
+      key: "day",
+      label: "DAY",
+    },
+    {
+      key: "at9",
+      label: "9:00-10:00",
+    },
+    {
+      key: "at10",
+      label: "10:00-11:00",
+    },
+    {
+      key: "at11",
+      label: "11:00-12:00",
+    },
+    {
+      key: "at12",
+      label: "12:00-1:00",
+    },
+    {
+      key: "at1",
+      label: "1:00-2:00",
+    },
+    {
+      key: "at2",
+      label: "2:00-3:00",
+    },
+    {
+      key: "at3",
+      label: "3:00-4:00",
+    },
+    {
+      key: "at4",
+      label: "4:00-5:00",
+    },
+    {
+      key: "at5",
+      label: "5:00-6:00",
+    },
+  ];
 
-    const [value, setValue] = useState("");
-    const columns = [
-        {
-            key: "day",
-            label: "DAY",
-        },
-        {
-            key: "at9",
-            label: "9:00-10:00",
-        },
-        {
-            key: "at10",
-            label: "10:00-11:00",
-        },
-        {
-            key: "at11",
-            label: "11:00-12:00",
-        },
-        {
-            key: "at12",
-            label: "12:00-1:00"
-        },
-        {
-            key: "at1",
-            label: "1:00-2:00",
-        },
-        {
-            key: "at2",
-            label: "2:00-3:00"
-        },
-        {
-            key: "at3",
-            label: "3:00-4:00",
-        },
-        {
-            key: "at4",
-            label: "4:00-5:00",
-        }, {
-            key: "at5",
-            label: "5:00-6:00"
-        }
-    ];
-
-
-    return (
-        <div id="Zoom-Content" className="w-full h-full flex items-center">
-
-            {
-                value && <div className="top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 absolute  backdrop-blur-lg w-full h-full flex items-center text-xl justify-center flex-col">
-                    <div className=" grid gap-4 border-2 rounded-xl p-2 ">
-                        <p className="p-2 text-center flex items-center justify-center">{value}</p>
-                        <h1 className="cursor-pointer bg-blue-400 rounded-lg p-2  text-black flex items-center justify-center " onClick={() => {
-                            setValue("")
-                        }}>Close</h1>
-
-                    </div>
-                </div>
-            }
-            <Table className="w-full" aria-label="Example table with dynamic content">
-                <TableHeader className=" w-full h-full">
-                    {columns.map((column) =>
-                        <TableColumn className="text-xl text-blue-400 p-6  font-extrabold gap-2" key={column.key}>{column.label}</TableColumn>
-                    )}
-                </TableHeader>
-                <TableBody className="w-full h-full" >
-                    {rows.map((row) =>
-                        <TableRow key={row.key}>
-                            {(columnKey) => <TableCell onClick={() => {
-                                setValue(getKeyValue(row, columnKey));
-                            }} className="text-xl rounded-md border-1 w-[232.42]     border-blue-400/20 p-4 pb-4 cursor-pointer items-center justify-center">{getKeyValue(row, columnKey)}</TableCell>}
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+  return (
+    <div id="Zoom-Content" className="w-full h-full flex items-center">
+      {value && (
+        <div className="top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 absolute  backdrop-blur-lg w-full h-full flex items-center text-xl justify-center flex-col">
+          <div className=" grid gap-4 border-2 sticky top-[60%] left-[50%] rounded-xl p-2 ">
+            <p className="p-2 text-center flex items-center justify-center">
+              {value}
+            </p>
+            <h1
+              className="cursor-pointer bg-blue-400 rounded-lg p-2  text-black flex items-center justify-center "
+              onClick={() => {
+                setValue("");
+              }}
+            >
+              Close
+            </h1>
+            <style jsx global>
+              {"body { width: 100%; height: 100vh; overflow: hidden; }"}
+            </style>
+          </div>
         </div>
-    )
-}
+      )}
+      <Table className="w-full" aria-label="Example table with dynamic content">
+        <TableHeader className=" w-full h-full">
+          {columns.map((column) => (
+            <TableColumn
+              className="text-xl text-blue-400 p-6  font-extrabold gap-2"
+              key={column.key}
+            >
+              {column.label}
+            </TableColumn>
+          ))}
+        </TableHeader>
+        <TableBody className="w-full h-full">
+          {rows.map((row) => (
+            <TableRow key={row.key}>
+              {(columnKey) => (
+                <TableCell
+                  onClick={() => {
+                    setValue(getKeyValue(row, columnKey));
+                  }}
+                  className="text-xl rounded-md border-1 w-[232.42]     border-blue-400/20 p-4 pb-4 cursor-pointer items-center justify-center"
+                >
+                  {getKeyValue(row, columnKey)}
+                </TableCell>
+              )}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
-export {
-    TimeTableUI
-}
+export { TimeTableUI };
