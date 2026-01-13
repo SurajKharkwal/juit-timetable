4aeeaa58430ab51b563cca91e012d96462f4d466
SurajKharkwal <kharkwalsuraj13@gmail.com>
2026-01-14 01:04:27 +0530

    Fix: minor changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 import DefaultLayout from '@/layouts'
 import { Provider } from '@/components/provider'
 
